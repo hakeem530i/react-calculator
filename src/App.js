6ba240ce261e92edf2b7ef7eaa28a241444dd4ac
thechocolateman <hakeemjgreen@gmail.com>
2019-03-04 14:42:04 -0500

    Create layout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      screen: 0
+    };
+  }
+
+  handleChange() {}
+  handleNumClick(num) {
+    this.setState({ screen: num });
+    //alert(num);
+  }
+  buttonRender() {
+    let numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+    let buttonList = numbers.map(num => {
+      return (
+        <button
+          key={num}
+          onClick={() => this.handleNumClick(num)}
+          className="w-33 f2 bg-animate hover-bg-black hover-white bg-transparent bn"
+        >
+          {num}
+        </button>
+      );
+    });
+    return buttonList;
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/Appp.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="calc-container" className="container center pa2 vh-80">
+          <input
+            className="w-100 f1 tr"
+            type="text"
+            value={this.state.screen}
+          />
+          {this.buttonRender()}
+        </div>
       </div>
     );
   }
